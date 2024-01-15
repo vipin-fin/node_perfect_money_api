d519bf36fb492f0aacaf0dc1bc104e0922a1e167
vipin-fin <vkumar1@stackflow.org>
2024-01-15 11:31:57 +0530

    Update to check V2-hash Code

--- md5.js
+++ md5.js
@@ -15,13 +15,29 @@ console.log("🚀 ~ file: md5.js:11 ~ reference:", reference)
 let timestamp = Math.floor(moment().format('x') / 1000);
 console.log("🚀 ~ file: md5.js:15 ~ timestamp:", timestamp)
 
+let PAYMENT_ID = '20079-1705060053';
+let PAYEE_ACCOUNT = 'U42541229';
+let PAYMENT_AMOUNT = '0.1';
+let PAYMENT_UNITS = 'USD';
+let PAYMENT_BATCH_NUM = '563651604';
+let PAYER_ACCOUNT = 'U45461501';
+let AlternateMerchantPassphraseHash = 'E4815FLkE6HhacquHhWuRHDnP';  // This should be Md5 value of original AltPassphrase
+let TIMESTAMPGMT = '1705059816';
 // PAYMENT_ID:PAYEE_ACCOUNT:PAYMENT_AMOUNT:PAYMENT_UNITS:PAYMENT_BATCH_NUM:PAYER_ACCOUNT:AlternateMerchantPassphraseHash:TIMESTAMPGMT
 // BA-103:U40040094:0.01:USD:540185452:U45461501:35T6n36cTBvDZMeapOAzyNhCp:1694429998
-// BA-103:U40040094:0.01:USD:540185452:U45461501:D7AA73CD7F8A2C0025CBAE43CF1FE29C:1694429998
+
 // Example usage
-const inputString = 'BA-103:U40040094:0.01:USD:540185452:U45461501:D7AA73CD7F8A2C0025CBAE43CF1FE29C:1694429998';
-// const inputString = '35T6n36cTBvDZMeapOAzyNhCp';
-const md5Hash = generateMD5Hash(inputString);
+AlternateMerchantPassphraseHash = generateMD5Hash(AlternateMerchantPassphraseHash).toUpperCase();
+console.log("==>>>==>>>==vk==>>> ~ file: md5.js:31 ~ AlternateMerchantPassphraseHash~", AlternateMerchantPassphraseHash)
+
+let inputString = PAYMENT_ID + ':' + PAYEE_ACCOUNT + ':' + PAYMENT_AMOUNT + ':' + PAYMENT_UNITS + ':' + PAYMENT_BATCH_NUM + ':' + PAYER_ACCOUNT + ':' + AlternateMerchantPassphraseHash + ':' + TIMESTAMPGMT;
+console.log("==>>>==>>>==vk==>>> ~ file: md5.js:32 ~ inputString~", inputString)
+
+// inputString = '20079-1705060053:U42541229:0.1:USD:563651604:U45461501:7C3C95C8B34D40AAC5CAB38CFD8A3031:1705059816'
+
+// {"PAYEE_ACCOUNT":"U42541229","PAYMENT_ID":"20079-1705060053","PAYMENT_AMOUNT":"0.1","PAYMENT_UNITS":"USD","PAYMENT_BATCH_NUM":"563651604","PAYER_ACCOUNT":"U45461501","TIMESTAMPGMT":"1705059816","V2_HASH":"A9BD236257A4430F25A04215C31CDB7B","BAGGAGE_FIELDS":""}
+
+const md5Hash = generateMD5Hash(inputString).toUpperCase();
 console.log(`MD5 Hash for '${inputString}': ${md5Hash}`);
 
 
